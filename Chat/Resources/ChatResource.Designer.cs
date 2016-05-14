e69a46a71501783e0c87032e71679593c475cb5e
Nail Salikhov <nail.salikhov@hotmail.com>
2016-05-14 13:05:10 +0300

    Chat resource renamed.

--- Chat/Resources/ChatResource.Designer.cs
+++ Chat/Resources/ChatResource.Designer.cs
@@ -22,14 +22,14 @@ namespace Chat.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Chat {
+    public class ChatResource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Chat() {
+        internal ChatResource() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Chat.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Chat.Resources.Chat", typeof(Chat).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Chat.Resources.ChatResource", typeof(ChatResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
